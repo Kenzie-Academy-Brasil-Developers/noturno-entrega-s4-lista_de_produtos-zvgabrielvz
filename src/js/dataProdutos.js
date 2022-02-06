2080b296c5d7cd7373c60b28e1e9eb7c6ea2e875
Gabriel Silva <gabriel_santos2006@hotmail.com>
2022-02-06 15:27:54 -0300

    finshed project

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,60 @@
 const produtos = [
     {
-        nome: 'Banana',
+        nome: 'banana',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
         img: "./src/img/banana.png"
     },
     {
-        nome: 'Morango',
+        nome: 'morango',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
         img: './src/img/morango.png'
     },
     {
-        nome: 'Maçã',
+        nome: 'maçã',
         preco: 2,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maça.png'
+        img: './src/img/maca.png'
     },
     {
-        nome: 'Pão',
+        nome: 'pão',
         preco: 4,
         secao: 'Panificadora',
         categoria: 'Pães',
         img: './src/img/pao.png'
     },
     {
-        nome: 'Leite',
+        nome: 'leite',
         preco: 5,
         secao: 'Laticínio',
         categoria: 'Leite',
         img: './src/img/leite.png'
+    },
+    {
+        nome: 'creme de leite',
+        preco: 6.50,
+        secao: 'Laticínio',
+        categoria: 'Leite',
+        img: './src/img/cremedeleite.png'
+    },
+    {
+        nome: 'queijo',
+        preco: 15,
+        secao: 'Laticínio',
+        categoria: 'Queijo',
+        img: './src/img/queijo.png'
+    },
+    {
+        nome: 'iogurte',
+        preco: 3,
+        secao: 'Laticínio',
+        categoria: 'Leite',
+        img: './src/img/iogurte.png'
     }
 ]
+
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,99 @@
+// Selecionando elemento ul do HTML
+const ul = document.querySelector('.containerListaProdutos ul');
+
+
+
+function montarListaProdutos(listaProdutos) {
+    let somaValor = 0; 
+    ul.innerHTML = '';
+    listaProdutos.forEach((produto) => {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        const span = document.createElement('span');
+
+        // Adicionando dados do produto aos elementos
+        img.src = produto.img;
+        img.alt = produto.nome;
+        h3.innerText = produto.nome;
+        p.innerText = produto.preco;
+        span.innerText = produto.secao;
+
+        // Adicionando o elementos para o li
+        li.appendChild(img);
+        li.appendChild(h3);
+        li.appendChild(p);
+        li.appendChild(span);
+
+        // Adicionando li ao HTML
+        ul.appendChild(li);
+
+        // Calculando Preço
+        let preco = produto.preco;
+        somaValor += preco;
+        console.log(somaValor)
+    });
+    const campoPreco = document.getElementById('precoTotal');
+    campoPreco.innerHTML = somaValor.toFixed(2);
+}
+
+function filtrarPorHortifruti() {
+    const listaHortifruti = produtos.filter((produto) => {
+        return produto.secao === 'Hortifruti';
+    });
+
+    montarListaProdutos(listaHortifruti);
+
+
+}
+
+
+function filtrarPorLaticinio() {
+    const listaLaticinio = produtos.filter((produto) => {
+        return produto.secao === 'Laticínio';
+    });
+
+    montarListaProdutos(listaLaticinio);
+
+    // Selecionando botao em nosso HTML
+   
+}
+ const botaoMostrarLaticinio = document.querySelector('.estiloGeralBotoes--filtrarLaticinio');
+
+    // Adicionando event listener de clique, e executando a função de filtro
+    botaoMostrarLaticinio.addEventListener('click', filtrarPorLaticinio);
+
+const buttonShowAll = document.getElementById('showAll');
+const buttonFilterHortifruti = document.getElementById('hortifruti');
+
+
+
+buttonShowAll.addEventListener("click", function(){
+    montarListaProdutos(produtos)
+});
+
+
+buttonFilterHortifruti.addEventListener("click", filtrarPorHortifruti);
+
+function textoNome() {
+    const inputNome = document.getElementById('inputNome');
+    const textName = inputNome.value;
+    return textName
+}
+
+
+function filtrarPorNome() {
+    const inputNome = document.getElementById('inputNome').value.toLowerCase();
+    const listaNome = produtos.filter((produto) => {
+        return produto.nome === inputNome;
+    });
+    montarListaProdutos(listaNome);
+}
+const botaoFiltraNome = document.querySelector('.estiloGeralBotoes--botaoBuscaPorNome');
+
+botaoFiltraNome.addEventListener('click', filtrarPorNome);
+
+
+
 
